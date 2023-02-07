eb1806263fd8f4845422cc4148941075c1218d72
TakanoriShima <quark2galaxy@gmail.com>
2023-02-07 09:32:29 +0000

    完成

---
+++ app/controllers/todolists_controller.rb
@@ -0,0 +1,55 @@
+class TodolistsController < ApplicationController
+  def new
+    # Viewへ渡すためのインスタンス変数に空のモデルオブジェクトを生成する。
+    @list = List.new
+  end
+    # ---- ここからコードを書きましょう ---- #
+  def create
+    @list = List.new(list_params)
+    if @list.save
+      # flashというハッシュに :notice をいうキーで '投稿が完了しました！' という文字列の値を保存
+      flash[:notice] = '投稿が完了しました！'
+      redirect_to todolist_path(@list.id)
+    else
+      render :new
+    end
+  end
+  def index
+    @lists = List.all
+  end
+
+  def show
+    @list = List.find(params[:id])
+  end
+
+  def edit
+    @list = List.find(params[:id])
+  end
+
+  def update
+
+    # ---- ここからコードを書きましょう ---- #
+    list = List.find(params[:id])
+    list.update(list_params)
+    redirect_to todolist_path(list.id)
+    # ---- ここまでのあいだにコードを書きましょう ---- #
+
+  end
+  def destroy
+
+    # ---- ここからコードを書きましょう ---- #
+    list = List.find(params[:id])  # データ（レコード）を1件取得
+    list.destroy  # データ（レコード）を削除
+    redirect_to todolists_path  # 投稿一覧画面へリダイレクト
+    # ---- ここまでのあいだにコードを書きましょう ---- #
+
+  end
+
+  private
+  # ストロングパラメータ
+  def list_params
+    # ---- 下記1行を変更してください ---- #
+    params.require(:list).permit(:title, :body, :image)
+  end
+  # ---- ここまでのあいだにコードを書きましょう ---- #
+end
---
+++ config/initializers/application_controller_renderer.rb
@@ -0,0 +1,9 @@
+# Be sure to restart your server when you modify this file.
+
+# ActiveSupport::Reloader.to_prepare do
+#   ApplicationController.renderer.defaults.merge!(
+#     http_host: 'example.org',
+#     https: false
+#   )
+# end
+Refile.secret_key = '543fb6cd788cbafeced7cc2bb6dbed1c14c9f5a382170a371fb6064f6ad7b3efecaa965f5ba264329c5be604a25a64ee89a84f22700a4969975b95a007386f02'
